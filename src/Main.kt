d1e3b82c00bfe8355d468c7e20a455ed8fae358d
Valentine-Nkatha <valentinnenkatha@gmail.com>
2024-03-25 08:25:56 +0300

    added files

---
+++ src/Main.kt
@@ -0,0 +1,38 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    barnieBake("Barnie bakes brown bagels")
+    allElements(arrayOf(1,2,3,4,5))
+    calculateVolume()
+}
+fun barnieBake(sentence:String){
+  var statement="arnie akes rown agels"
+    println(statement)
+}
+fun allElements(args:Array<Int>):Int{
+var b=arrayOf(1,2,3,4,5)
+ var c= b.sum()
+ var d= b.count()
+
+
+
+
+    println(c)
+    println(d)
+
+
+
+    return c
+    return d
+
+
+
+}
+fun calculateVolume(){
+    var r=3
+    var volume=4/3*3.14159*3*3*3
+    println(volume)
+}
+//fun isPalindrome(word:String):Boolean{
+//
+//}
